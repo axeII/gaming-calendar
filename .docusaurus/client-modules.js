bace7a0f4c0364b160d9a790431a402f35bcc2d0
axeII <axeII@users.noreply.github.com>
2026-06-14 12:38:23 +0000

    deploy: ca9f070dde0d585c0039d144b34d961736a1aebd

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/ales/git/gaming-calendar/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/ales/git/gaming-calendar/node_modules/infima/dist/css/default/default.css"),
-  require("/home/ales/git/gaming-calendar/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/ales/git/gaming-calendar/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/ales/git/gaming-calendar/src/css/custom.css"),
-];
